b7bdb4025c403dfcc74e48d1694c4ce3464f0fb0
Xuan Hoang Nguyen <xnguyen-24@gpu1.enst.fr>
2025-04-01 15:12:34 +0200

    reorganize folder metrics

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # 2025_XAI
+-- Metrics: nên sử dụng class kế thừa để dễ quản lý các metrics, có chung cấu trúc từ lớp cha 
+Vấn đề	nếu k có 
+❌ Không có chuẩn chung	Không đảm bảo class nào cũng có __call__() hoặc name
+❌ Khó quản lý hàng loạt	Không thể xử lý qua vòng lặp for metric in metrics: một cách an toàn
+❌ Không thể kiểm tra thống nhất	Không thể dùng isinstance(metric, BaseMetric) để kiểm tra loại
+❌ Khó mở rộng	Không tận dụng được tính kế thừa, phải sửa tay từng class
+❌ Khó bảo trì	Đọc code khó biết class nào là "metric", class nào là thứ khác
--- metrics/__init__.py
+++ metrics/__init__.py
@@ -1,9 +1,2 @@
-from .metric import calculate_metrics
-from .average_drop import AverageDrop
-from .increase_in_confidence import IncreaseInConfidence
-from .deletion_curve import DeletionCurveAUC
-from .insertion_curve import InsertionCurveAUC
-from .infidelity import Infidelity, perturb_fn
-from .sensitivity import Sensitivity
-from .road import RoadCombined
-from .roc_auc import ROC_AUC
+from .test_metrics import calculate_metrics
+from .metrics import AverageDrop, AverageIncrease, DeletionCurveAUC, InsertionCurveAUC, Infidelity, perturb_fn, Sensitivity, RoadCombined, ROC_AUC
